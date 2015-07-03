114fcd4e78393554734cb7796152d3ac5f9c4fc7
test-china <testerhome@126.com>
2015-07-03 19:09:05 +0800

    backup

--- config.rb
+++ config.rb
@@ -78,6 +78,6 @@ end
 
 activate :deploy do |deploy|
   deploy.method = :git
-  deploy.remote = 'git@gitcafe.com:lihuazhang/lihuazhang.git'
-  deploy.branch = 'gitcafe-pages'
+  deploy.remote = 'git@github_testerhome.com:test-china/test-china.git'
+  deploy.branch = 'gh-pages'
 end
